58c93af8632b1afeb57ba069527f425f0f3bffc6
kellton42 <parvinder.singh@kelltontech.com>
2021-02-07 14:38:44 +0530

    Updates

---
+++ precache-manifest.6c152bedcd182415da064d78311f9d9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c30b6cc8306ac6a7a846cbdd4f927b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b35ff3455d4d6a9718",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4534b9227734c5e5dff4",
+    "url": "/static/css/main.c9ecbc85.chunk.css"
+  },
+  {
+    "revision": "53b35ff3455d4d6a9718",
+    "url": "/static/js/2.d6fc278b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d6fc278b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4534b9227734c5e5dff4",
+    "url": "/static/js/main.d8c911d1.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "4c07371e92262708f972a95f5b22dcba",
+    "url": "/static/media/loader.4c07371e.gif"
+  },
+  {
+    "revision": "46d4f6e4178696a65601bc6b31c234f5",
+    "url": "/static/media/no-data.46d4f6e4.png"
+  }
+]);
